c8a12748cedcd009c0e215dc6466e1602f6064a1
Edward Smith <edward.m.smith80@gmail.com>
2020-08-25 20:54:19 +0000

    spinner1.js uses an array of characters for a terminal animation. the intention at this point in LHL was to do it by hand (no for loop) but I thought I would try it with a for loop

---
+++ spinner1.js
@@ -0,0 +1,19 @@
+const spinner = ['\r|', '\r/', '\r-', '\r\\'];
+let timer = 100;
+for (let i = 0; i < 3; i++) {
+  
+  for (let char of spinner) {
+    timer += 200;
+    setTimeout(() => {
+      process.stdout.write(`${char}   `);
+    }, timer);
+  }
+}
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, timer += 200);
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, timer);
